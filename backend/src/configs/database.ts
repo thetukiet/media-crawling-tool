fa6fb0ca2ae57aae7f404b3a50425a1f692ce51a
Hien Nguyen <hien@devinci.io>
2024-07-01 20:31:35 +0700

    Update build source

--- backend/src/configs/database.ts
+++ backend/src/configs/database.ts
@@ -1,20 +1,15 @@
 import { DataSource } from "typeorm";
 import dotenv from "dotenv";
+import {Link} from "../entity/Link";
 
 dotenv.config();
 
-// DATABASE_URL=postgres://admin:admin123@localhost:5432/media
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "admin",
-    password: "admin123",
-    database: "media",
     url: process.env.DATABASE_URL,
     synchronize: false,
     logging: false,
-    entities: ["src/entity/**/*.ts"],
+    entities: [Link],
     migrations: ["src/migrations/**/*.ts"],
     subscribers: ["src/subscribers/**/*.ts"],
 });
